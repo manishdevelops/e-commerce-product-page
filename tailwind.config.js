831a91158374b3ba5596abe58b7a6452879e84d2
manish mandal <76776194mm@gmail.com>
2023-05-21 23:46:30 +0530

    modified

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,html}'],
+	theme: {
+		extend: {
+			colors: {
+				Orange: 'hsl(26, 100%, 55%)',
+				'Pale-orange': 'hsl(25, 100%, 94%)',
+				'very-dark-blue': 'hsl(220, 13%, 13%)',
+				'dark-grayish-blue': 'hsl(219, 9%, 45%)',
+				'grayish-blue': 'hsl(220, 14%, 75%)',
+				'light-grayish-blue': 'hsl(223, 64%, 98%)',
+				'black-opacity-75': 'rgba(0, 0, 0, 0.75)',
+			},
+			fontFamily: {
+				'kumbh-sans': ['Kumbh Sans', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+};
