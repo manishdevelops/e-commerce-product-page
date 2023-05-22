954df69cff7c08dccea731f7bef9b7b7b419aed9
manish mandal <76776194mm@gmail.com>
2023-05-22 18:33:31 +0530

    add body

---
+++ src/components/Body.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProductShow from './ProductShow';
+
+const Body = () => {
+	return (
+		<main className='my-10  md:w-[90%] lg:w-[75%] mx-auto'>
+			<ProductShow />
+			<section>
+				<h1>sneaker company</h1>
+				<h2>fall limited edition sneakers</h2>
+			</section>
+		</main>
+	);
+};
+
+export default Body;
