7ee86fcc4f6c000aba5b6a49734877f30288d780
manish mandal <76776194mm@gmail.com>
2023-05-23 14:28:55 +0530

    lifting state up for menu handle

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,12 @@ import avatar from '../assets/images/image-avatar.png';
 import close from '../assets/images/icon-close.svg';
 import logo from '../assets/images/logo.svg';
 import Delete from '../assets/images/icon-delete.svg';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
 import { clearCart } from '../utils/cartSlice';
+import { closeMenu, toggleMenu } from '../utils/appSlice';
 
 const Header = () => {
-	const [isNavOpen, setIsNavOpen] = useState(false);
 	const [isCartOpen, setIsCartOpen] = useState(false);
 
 	const cartItems = useSelector((store) => store.cart.items);
@@ -21,18 +20,21 @@ const Header = () => {
 		dispatch(clearCart());
 	};
 
+	const toggleMenuHandler = function () {
+		dispatch(toggleMenu());
+	};
+	const closeMenuHandler = function () {
+		dispatch(closeMenu());
+	};
+
+	const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+
 	return (
 		<>
 			<header className='font-kumbh-sans flex md:w-[95%] lg:w-[85%] mx-auto mt-4 justify-between align-baseline px-5 pt-5'>
 				<div className='flex '>
 					<div className='flex mr-6 md:mr-10'>
-						<button
-							className='md:hidden'
-							onClick={() => {
-								setIsNavOpen(!isNavOpen);
-								setIsCartOpen(false);
-							}}
-						>
+						<button className='md:hidden' onClick={toggleMenuHandler}>
 							<img className='w-6 h-5  mr-4 block' alt='menu' src={menu} />
 						</button>
 						<span aria-label='logo'>
@@ -44,16 +46,11 @@ const Header = () => {
 						</span>
 					</div>
 					<div
-						className={`w-[70%] absolute z-10 md:static md:block h-full bg-white  left-0 top-0 px-3 mt-4 md:mt-0 ${
-							isNavOpen ? 'block' : 'hidden'
+						className={`w-[70%] absolute z-20 md:static md:block h-full bg-white  left-0 top-0 px-3 mt-4 md:mt-0 ${
+							isMenuOpen ? 'block' : 'hidden'
 						}`}
 					>
-						<button
-							className='md:hidden'
-							onClick={() => {
-								setIsNavOpen(false);
-							}}
-						>
+						<button className='md:hidden' onClick={closeMenuHandler}>
 							<img alt='close' src={close} className='my-7 w-5 mx-4' />
 						</button>
 						<nav>
@@ -61,7 +58,7 @@ const Header = () => {
 								<li className='capitalize  font-bold text-xl mt-3 mx-4 md:mx-0  md:text-sm  cursor-pointer hover:text-very-dark-blue md:text-center'>
 									<a
 										href='#collections'
-										className="h-20 after:content-[''] after:bg-orange-1 after:w-[72.94px] md:hover:after:h-[3px]  after:top-4 flex flex-col justify-between"
+										className="h-20 after:content-[''] after:bg-orange-1 after:w-[72.94px] md:hover:after:h-[3px]  after:top-4 flex flex-col justify-between duration-200"
 									>
 										collections
 									</a>
@@ -69,7 +66,7 @@ const Header = () => {
 								<li className='capitalize mt-3 mx-4 md:mx-0  md:text-sm font-bold text-xl cursor-pointer hover:text-very-dark-blue md:text-center'>
 									<a
 										href='#men'
-										className="h-20 after:content-[''] after:bg-orange-1 after:w-[72.94px] md:hover:after:h-[3px]  after:top-4 flex flex-col justify-between"
+										className="h-20 after:content-[''] after:bg-orange-1 after:w-[72.94px] md:hover:after:h-[3px]  after:top-4 flex flex-col justify-between duration-200"
 									>
 										men
 									</a>
@@ -77,7 +74,7 @@ const Header = () => {
 								<li className='capitalize mt-3 mx-4 md:mx-0  md:text-sm font-bold text-xl cursor-pointer hover:text-very-dark-blue md:text-center'>
 									<a
 										href='#women'
-										className="h-20 after:content-[''] after:bg-orange-1 after:w-[72.94px] md:hover:after:h-[3px]  after:top-4 flex flex-col justify-between"
+										className="h-20 after:content-[''] after:bg-orange-1 after:w-[72.94px] md:hover:after:h-[3px]  after:top-4 flex flex-col justify-between duration-200"
 									>
 										women
 									</a>
@@ -85,7 +82,7 @@ const Header = () => {
 								<li className='capitalize mt-3 mx-4 md:mx-0 md:text-sm font-bold text-xl cursor-pointer hover:text-very-dark-blue md:text-center'>
 									<a
 										href='#about'
-										className="h-20 after:content-[''] after:bg-orange-1 after:w-[72.94px] md:hover:after:h-[3px]  after:top-4 flex flex-col justify-between"
+										className="h-20 after:content-[''] after:bg-orange-1 after:w-[72.94px] md:hover:after:h-[3px]  after:top-4 flex flex-col justify-between duration-200"
 									>
 										about
 									</a>
@@ -93,7 +90,7 @@ const Header = () => {
 								<li className='capitalize mt-3 mx-4 md:mx-0 md:text-sm font-bold text-xl cursor-pointer hover:text-very-dark-blue md:text-center'>
 									<a
 										href='#contact'
-										className="h-20 after:content-[''] after:bg-orange-1 after:w-[72.94px] md:hover:after:h-[3px]  after:top-4 flex flex-col justify-between"
+										className="h-20 after:content-[''] after:bg-orange-1 after:w-[72.94px] md:hover:after:h-[3px]  after:top-4 flex flex-col justify-between duration-200"
 									>
 										contact
 									</a>
@@ -106,7 +103,6 @@ const Header = () => {
 					<button
 						onClick={() => {
 							setIsCartOpen(!isCartOpen);
-							setIsNavOpen(false);
 						}}
 						className='flex'
 					>
@@ -118,7 +114,7 @@ const Header = () => {
 						<img alt='cart' src={cart} className='w-6 h-6 mr-3 md:mr-6' />
 					</button>
 					{isCartOpen && (
-						<article className='absolute z-[20] top-32 md:top-20 left-[5vw] md:left-auto w-[90vw] md:right-[5%] lg:right-[6%]  md:w-80 h-56 bg-White-1 shadow-xl p-6 rounded-md'>
+						<article className='absolute z-10 top-32 md:top-20 left-[5vw] md:left-auto w-[90vw] md:right-[5%] lg:right-[6%]  md:w-80 h-64 bg-White-1 shadow-xl p-[6vw] md:p-6 rounded-md'>
 							<p className='p-4'>cart</p>
 							<hr />
 							{(cartItems?.[0]?.totalItem === 0 || cartItems.length === 0) && (
@@ -128,15 +124,15 @@ const Header = () => {
 							)}
 							{cartItems?.[0]?.totalItem > 0 && (
 								<>
-									<div className='flex justify-between my-4'>
+									<div className='flex justify-center my-4 leading-8'>
 										<span aria-label='product-icon'>
 											<img
 												src={cartItems[0].url}
 												alt='product-icon'
-												className='w-10 h-10 mr-2 rounded-md'
+												className='h-14 md:w-10 md:h-10 mr-2 rounded-md'
 											/>
 										</span>
-										<div>
+										<div className='mx-4 md:mx-2'>
 											<p className='capitalize text-sm'>{cartItems[0].desc}</p>
 											<p>
 												$125 x {cartItems?.[0]?.totalItem}{' '}
@@ -153,7 +149,7 @@ const Header = () => {
 											/>
 										</button>
 									</div>
-									<button className='w-full capitalize font-bold text-White-1 bg-orange-1 rounded-md py-2 hover:opacity-50 duration-200'>
+									<button className='w-full capitalize font-bold text-White-1 bg-orange-1 rounded-md py-3 md:py-2 hover:opacity-50 duration-200'>
 										checkout
 									</button>
 								</>
