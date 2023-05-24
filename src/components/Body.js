65b80a7c42dff5f58f58b46a605cdf9b53cdb406
manish mandal <76776194mm@gmail.com>
2023-05-24 10:11:52 +0530

    add bg for mob menu

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,12 +6,16 @@ import { useSelector } from 'react-redux';
 
 const Body = () => {
 	const isLightboxOpen = useSelector((state) => state.lightbox.isLightboxOpen);
+	const isMenuOpen = useSelector((state) => state.app.isMenuOpen);
 
 	return (
 		<main className='my-10  md:w-[90%] lg:w-[75%] mx-auto font-kumbh-sans md:flex md:justify-between'>
 			<ProductShow />
 			<ProductInfo />
 			{isLightboxOpen && <Lightbox />}
+			{isMenuOpen && (
+				<section className='fixed z-20 w-full h-screen left-0 top-0 bg-black-opacity-75 md:hidden'></section>
+			)}
 		</main>
 	);
 };
